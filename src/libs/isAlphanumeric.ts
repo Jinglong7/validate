286332a63f21a4c53177563dda3c1e586f4ca0b5
Bunlong <bunlong.van@gmail.com>
2020-09-29 09:28:17 +0700

    :heavy_plus_sign: isAlphanumeric

---
+++ src/libs/isAlphanumeric.ts
@@ -0,0 +1,14 @@
+// @ts-ignore allowing typedoc to build
+import assertString from '../utils/assertString.ts';
+// @ts-ignore allowing typedoc to build
+import { alphanumeric } from '../utils/alpha.ts';
+
+export const isAlphanumeric = (str: string, locale = 'en-US') => {
+  assertString(str);
+  if (locale in alphanumeric) {
+    return (alphanumeric as any)[locale].test(str);
+  }
+  throw new Error(`Invalid locale '${locale}'`);
+};
+
+export const alphanumericLocales = Object.keys(alphanumeric);
--- src/mod.ts
+++ src/mod.ts
@@ -3,4 +3,6 @@ export * from './libs/isAfter.ts';
 // @ts-ignore allowing typedoc to build
 export * from './libs/isAlpha.ts';
 // @ts-ignore allowing typedoc to build
+export * from './libs/isAlphanumeric.ts';
+// @ts-ignore allowing typedoc to build
 export * from './libs/isBoolean.ts';
